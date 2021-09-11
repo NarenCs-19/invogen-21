94a9522f38f0ff21f6e0f0858f62b86514699a3e
NarenCs-19 <narendirakumar2002@gmail.com>
2021-09-11 11:26:27 +0530

    procfile deleted

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './Components/App';
 import './assets/bootstrap/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
     <App />,
